724c9b1d86cdf82b0ddd8ababac3fff1c19b870c
ExesoftInc <exesoftinc@gmail.com>
2022-05-10 12:06:57 -0500

    Add project files.

---
+++ Services/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SalesAPI.Services
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
